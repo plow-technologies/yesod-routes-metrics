627db18195e093131912e55d82cb5785eceadf04
James Haver II <mchaver@gmail.com>
2017-09-19 17:00:21 +0800

    Start adding latencies

--- src/Yesod/Routes/Metrics.hs
+++ src/Yesod/Routes/Metrics.hs
@@ -20,19 +20,30 @@ import           Control.Monad   (forM)
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as C
 import           Data.Char (isUpper)
+import           Data.Int (Int64)
+import           Data.Maybe (maybe)
 import           Data.Monoid     ((<>))
 import qualified Data.Map.Strict as Map
 import           Data.Text       (Text)
 import qualified Data.Text       as T
+import           Data.Time
 import           Network.HTTP.Types.Status   (statusCode)
 import           Network.Wai
 import           System.Metrics
 import qualified System.Metrics.Counter as Counter
+import qualified System.Metrics.Gauge   as G
 import           Yesod.Routes.TH.Types (ResourceTree)
 
+import Data.IORef (IORef, atomicModifyIORef, newIORef, readIORef)
+
 data YesodMetrics = 
-  YesodMetrics 
-    { routeCounters :: Map.Map String Counter.Counter
+  YesodMetrics
+    { routeCounters         :: Map.Map String Counter.Counter
+    , routesTotalRequests   :: Counter.Counter
+    , routeMaxLatencies     :: Map.Map String G.Gauge
+    , routeMinLatencies     :: Map.Map String G.Gauge
+    , routeAverageLatencies :: Map.Map String G.Gauge
+    , routeLatencySum       :: Map.Map String (IORef Int64)  -- used for calculation only with its routeCounter to calculate average
     }
 
 data YesodMetricsConfig =
@@ -75,7 +86,7 @@ registerYesodMetrics config routesFileContents store = do
 
 registerYesodMetricsWithResourceTrees :: YesodMetricsConfig -> [ResourceTree String] -> Store -> IO YesodMetrics
 registerYesodMetricsWithResourceTrees ymc resources store = do
-  counters <- forM routes $ \route -> do 
+  counters <- forM routes $ \route -> do
     let namespacedRoute = namespace' <> (T.pack route)
     counter <- createCounter namespacedRoute store
     if verbose ymc
@@ -84,8 +95,13 @@ registerYesodMetricsWithResourceTrees ymc resources store = do
         return $ [(route, counter)] ++ zip ((T.unpack . (<>) (T.pack route)) <$> responseStatuses') counters
       else 
         return [(route, counter)]
-        
-  return $ YesodMetrics (Map.fromList $ concat counters)
+  
+  c <- createCounter (T.pack $ alterResponseStatus "total_requests_per_interval") store
+  avgs <- forM routes $ \route -> do
+    r <- newIORef 0
+    return (route,r)
+    
+  return $ YesodMetrics (Map.fromList $ concat counters) c Map.empty Map.empty Map.empty (Map.fromList avgs)
 
   where
     routes = (alterRouteName ymc) <$> convertResourceTreesToRouteNames resources
@@ -111,23 +127,31 @@ metrics ymc routesFileContents yesodMetrics app req respond =
     resources  = resourcesFromString . C.unpack $ routesFileContents
 
 metricsWithResourceTrees :: YesodMetricsConfig -> [ResourceTree String] -> YesodMetrics -> Middleware
-metricsWithResourceTrees ymc resources yesodMetrics app req respond = do
-  -- if the path in req corresponds to one of the Yesod routes
-  -- then update its corresponding counter
-  case mRouteName of
-    Nothing        -> return ()
-    Just routeName -> updateRoute $ (alterRouteName ymc) routeName
-          
-  app req $ respond'
+metricsWithResourceTrees ymc resources yesodMetrics app req respond = do  
+  before <- getCurrentTime
+  
+  app req $ respond' before
 
   where 
     mRouteName = convertRequestToRouteName req resources
 
-    respond' :: Response -> IO ResponseReceived
-    respond' res = do 
+    respond' :: UTCTime -> Response -> IO ResponseReceived
+    respond' before res = do 
+      -- if the path in req corresponds to one of the Yesod routes
+      -- then update its corresponding counter      
       case mRouteName of 
         Nothing -> return ()
-        Just routeName ->
+        Just routeName -> do
+          after <- getCurrentTime
+          
+          updateRoute ((alterRouteName ymc) routeName)
+          updateTotalRequests
+          
+          let diff = (round $ 1000 * (diffUTCTime after before))
+          updateMaxLatency ((alterRouteName ymc) routeName) diff
+          updateMinLatency ((alterRouteName ymc) routeName) diff
+          updateAverageLatency ((alterRouteName ymc) routeName) diff
+          
           case statusCode $ responseStatus res of
             s | s >= 500  -> updateRoute (routeName' <> (alterResponseStatus "_response_status_5xx"))
               | s >= 400  -> updateRoute (routeName' <> (alterResponseStatus "_response_status_4xx"))
@@ -145,4 +169,35 @@ metricsWithResourceTrees ymc resources yesodMetrics app req respond = do
     updateRoute name = 
       case Map.lookup name (routeCounters yesodMetrics) of
         Nothing -> return ()
-        Just counter -> Counter.inc counter
+        Just counter ->
+          Counter.inc counter
+    
+    updateTotalRequests = Counter.inc (routesTotalRequests yesodMetrics)
+    
+    updateMaxLatency :: String -> Int64 -> IO ()
+    updateMaxLatency name newLatency = do
+      maybe 
+        (return ())
+        (\gauge -> G.read gauge >>= \oldLatency -> if newLatency > oldLatency then G.set gauge newLatency else return ())
+        (Map.lookup name (routeMaxLatencies yesodMetrics))
+
+    updateMinLatency :: String -> Int64 -> IO ()
+    updateMinLatency name newLatency = 
+      maybe 
+        (return ())
+        (\gauge -> G.read gauge >>= \oldLatency -> if newLatency < oldLatency then G.set gauge newLatency else return ())
+        (Map.lookup name (routeMinLatencies yesodMetrics))
+    
+    updateAverageLatency :: String -> Int64 -> IO ()
+    updateAverageLatency name newLatency = do
+      case (,,) <$> mc <*> ms <*> ma of
+        Nothing -> return ()
+        Just (c,latencySumRef,avgGauge) -> do
+          t <- Counter.read c
+          atomicModifyIORef latencySumRef (\ls -> (ls + newLatency, ()))
+          latencySum <- readIORef latencySumRef
+          G.set avgGauge (round $ (fromIntegral latencySum :: Double) / (fromIntegral t :: Double))
+      where 
+        mc = (Map.lookup name (routeCounters   yesodMetrics))
+        ms = (Map.lookup name (routeLatencySum yesodMetrics))
+        ma = (Map.lookup name (routeAverageLatencies yesodMetrics))
